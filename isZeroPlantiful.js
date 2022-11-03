bdf183934c9b1fa0e23c90316e41e46feeb2d594
eliesiemi <69220191+eliesiemi@users.noreply.github.com>
2022-11-02 20:46:22 -0700

    Update isZeroPlantiful.js

--- isZeroPlantiful.js
+++ isZeroPlantiful.js
@@ -1,24 +1,28 @@
-function isZeroPlantiful (arr){
-      let count = 0
-      for(i=0;i<arr.length;i++){
-                  
-       
-        if(arr[i]==0){
-         
-          
-           count+=arr[i]
-           
-          count ++ 
-        } 
-      
-        if(count>=4){
-          return 1
-        } 
-        
-       
-      } 
-      
-      return 0
-        
+function zeroPlentiful(arr){
+  let count = 0
+  let total = 0
+  let foundNonZero = false
+  for (let i = 0; i<arr.length;i++){
+    if (arr[i] == 0){
+      count++
+    }
+    else{
+      if (count <= 4){
+        return 0
       }
-    console.log(isZeroPlantiful([0,0,0,0,0,1,]))
+      count = 0
+      total++
+    }
+  }
+
+  if (count <= 4){
+    return 0
+  }
+
+  if (foundNonZero == false){
+    return total+1
+  }
+}
+
+
+console.log(zeroPlentiful([0,0,0,0,0,1,0,0,0]))
