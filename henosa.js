6275adee547247999a96b8565af9f9a1faa81457
HENOS-art <73327914+HENOS-art@users.noreply.github.com>
2022-11-09 20:33:01 -0800

    Revert "add new html and js files"

--- henosa.js
+++
@@ -1,7 +0,0 @@
-function newWeb() {  
-    const userName=document.getElementById("userName") 
-    console.log("username",userName.value)
-    const passWord=document.getElementById("passWord") 
-    console.log("password",passWord.value)
-    }  
-    
