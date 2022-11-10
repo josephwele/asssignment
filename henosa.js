97ae09f630be10e3240a9a5924a4eb9312125357
HENOS-art <73327914+HENOS-art@users.noreply.github.com>
2022-11-09 16:00:28 -0800

    add new html and js files

---
+++ henosa.js
@@ -0,0 +1,7 @@
+function newWeb() {  
+    const userName=document.getElementById("userName") 
+    console.log("username",userName.value)
+    const passWord=document.getElementById("passWord") 
+    console.log("password",passWord.value)
+    }  
+    
