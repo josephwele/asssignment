cbb9414df0ee81c8db083a3a1d845de59e0ad52e
eliesiemi <69220191+eliesiemi@users.noreply.github.com>
2022-11-23 20:29:27 -0800

    Revert "Elyod"

---
+++ luwamTe_assign/signIn/scripte.js
@@ -0,0 +1,5 @@
+function validate(){
+    let username=document.getElementById("username").value
+    let password=document.getElementById("password").value
+    console.log("username",username,"password",password);
+}
